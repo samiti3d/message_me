4a62e7331de72ac4d054335fb8cd52731725e88e
samiti <samiti3d@gmail.com>
2019-01-31 18:05:58 +0700

    mount cable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  mount ActionCable.server, at: '/cable'
+
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
   
-  mount ActionCable.server, at: '/cable'
 
 end
