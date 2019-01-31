8419f040828fb9e577dfc4a28177e6bfe8334874
samiti <samiti3d@gmail.com>
2019-01-31 13:23:22 +0700

    final before upload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,23 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_buttom = function(){
+    if($('#messages').length > 0){
+        $('#messages').scrollTop($('#messages')[0].scrollHeight)
+    }
+    $('input[type="text"').val('');
+}
+
+submit_message = function(){
+    $('#message_body').on('keydown', function(e){
+        if(e.keyCode == 13){
+            $('button').click();
+            e.target.value = "";
+        }
+    });
+
+};
+
 $(document).on('turbolinks:load', function(){
 
     $('.ui.dropdown').dropdown();
@@ -27,5 +44,8 @@ $(document).on('turbolinks:load', function(){
             .closest('.message')
             .transition('fade');
     });
+
+    // submit_message();
+    scroll_buttom();
     
 })
