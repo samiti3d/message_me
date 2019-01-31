0b160f9b4e6dee507edc373999a06c2b809df536
samiti <samiti3d@gmail.com>
2019-01-31 20:58:14 +0700

    fix buy of current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
 
     def current_user
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        if !@current_user 
+            @current_user = nil
+        else
+            @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+
     end 
 
     def logged_in?
@@ -11,7 +16,7 @@ class ApplicationController < ActionController::Base
 
     def require_user
         if !logged_in?
-            flash[:error] = "กรุณาเข้าสู่ระบบก่อน"
+            # flash[:error] = "กรุณาเข้าสู่ระบบก่อน"
             redirect_to login_path
         end
     end
